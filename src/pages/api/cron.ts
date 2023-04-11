8e805ccc02f3928d3d46aa4ef22cd6041cd8e3c4
Jian <jiankuang7@gmail.com>
2023-04-11 14:40:36 -0400

    Updating logic for multiple locations

--- src/pages/api/cron.ts
+++ src/pages/api/cron.ts
@@ -9,27 +9,36 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const lat = "40.7376";
-  const lon = "-73.8789";
-  const dataResponse = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=081a314e861f49868f503b1ce665590b`
+  const locations = [{ lat: "40.7376", lon: "-73.8789", loc: "North Beach" }];
+
+  const responses = await Promise.all(
+    locations.map(async (loc) => {
+      const lat = loc.lat;
+      const lon = loc.lon;
+      const coord = `${lat},${lon}`;
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=081a314e861f49868f503b1ce665590b`
+      );
+      return [coord, (await response.json()) as Prisma.JsonObject];
+    })
   );
 
-  const weatherData = (await dataResponse.json()) as Prisma.JsonObject;
+  responses.map(async (weatherData) => {
+    try {
+      const coord = weatherData[0] as string;
+      await prisma.weather.update({
+        where: {
+          latLon: coord,
+        },
+        data: {
+          json: weatherData[1] as Prisma.JsonObject,
+        },
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Something went wrong" });
+    }
+  });
 
-  try {
-    const createdWeatherData = await prisma.weather.update({
-      where: {
-        latLon: `${lat},${lon}`,
-      },
-      data: {
-        json: weatherData,
-      },
-    });
-
-    res.status(200).json({ success: true, createdWeatherData });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Something went wrong" });
-  }
+  res.status(200).json({ message: "success", responses });
 }
