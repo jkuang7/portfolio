3e6f7a10a37d81ecfa5328ef76a0aaf195addb49
Jian <jiankuang7@gmail.com>
2023-04-11 12:00:19 -0400

    Removing old comment

--- src/pages/api/cron.ts
+++ src/pages/api/cron.ts
@@ -68,7 +68,3 @@ export default async function handler(
     res.status(500).json({ error: "Something went wrong" });
   }
 }
-
-// export async function handler(req: Request, res: Response): Promise<void> {
-
-// }
