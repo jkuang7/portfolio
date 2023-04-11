6c001f7c5a05f462bfd24c2fddf5424952359fb2
Jian <jiankuang7@gmail.com>
2023-04-11 12:04:00 -0400

    Updating lat and lon

--- src/pages/api/cron.ts
+++ src/pages/api/cron.ts
@@ -45,8 +45,8 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const lat = "40.7375751";
-  const lon = "-73.8788719";
+  const lat = "40.7376";
+  const lon = "-73.8789";
   const dataResponse = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=081a314e861f49868f503b1ce665590b`
   );
